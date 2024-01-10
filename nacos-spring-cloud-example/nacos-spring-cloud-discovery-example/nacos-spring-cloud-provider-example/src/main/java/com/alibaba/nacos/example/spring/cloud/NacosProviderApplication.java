c0e76d14689fd89731ee9ac9e00049824d7dde13
xionglei <xionglei@jiuqi.com.cn>
2024-01-10 17:45:11 +0800

    fix(nacos-spring-cloud): test spring cloud nacos server register and find

--- nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-provider-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosProviderApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-provider-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosProviderApplication.java
@@ -1,8 +1,11 @@
 package com.alibaba.nacos.example.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,9 +24,16 @@ public class NacosProviderApplication {
 
 	@RestController
 	class EchoController {
+		
+		@Value("${server.port}")
+		private String port;
+		
+		@Value("${userdomain}")
+		private String host;
+		
 		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
 		public String echo(@PathVariable String string) {
-			return "Hello Nacos Discovery " + string;
+			return "Hello Nacos Discovery " + string + "! this is server: " + host + ": " + port;
 		}
 	}
 }
