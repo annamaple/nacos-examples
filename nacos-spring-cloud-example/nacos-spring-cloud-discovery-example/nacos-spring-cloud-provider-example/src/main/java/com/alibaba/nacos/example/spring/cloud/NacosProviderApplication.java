2e6a3882fa968f1b5f2d31a61db5a94c6546ea65
xionglei <xionglei@jiuqi.com.cn>
2024-01-10 17:56:29 +0800

    fix(nacos-spring-cloud): deal with warnings

--- nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-provider-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosProviderApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-provider-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosProviderApplication.java
@@ -1,11 +1,9 @@
 package com.alibaba.nacos.example.spring.cloud;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
